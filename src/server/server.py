8a5910e1460c72508b6a7645824474e08597182f
Nagy Bence <nbence08@gmail.com>
2021-04-25 18:54:23 +0200

    Server class created

--- src/server/server.py
+++ src/server/server.py
@@ -1,9 +1,6 @@
 import os
 import sys
-import getopt
 from Crypto.PublicKey import RSA
-import time
-import pickle as pkl
 
 PACKAGE_PARENT = '..'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
@@ -14,85 +11,87 @@ from network.netinterface import network_interface
 # noinspection PyUnresolvedReferences
 from utils.keyExchange.keyExchangers import ServerKeyExchanger
 
-NET_PATH = os.path.abspath(os.path.join(
-    os.path.dirname(__file__), '..\\network\\traffic\\'))
-OWN_ADDR = 'B'
-print(NET_PATH)
-# ------------
-# main program
-# ------------
+class Server:
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], shortopts='hp:a:', longopts=[
-                               'help', 'path=', 'addr='])
-except getopt.GetoptError:
-    print('Usage: python receiver.py -p <network path> -a <own addr>')
-    sys.exit(1)
+    def __init__(self,
+                 netPath: str = "../network/files/",
+                 ownAddr: str = "A"):
+        self.netPath = netPath
+        self.ownAddr = ownAddr
 
-for opt, arg in opts:
-    if opt == '-h' or opt == '--help':
-        print('Usage: python receiver.py -p <network path> -a <own addr>')
-        sys.exit(0)
-    elif opt == '-p' or opt == '--path':
-        NET_PATH = arg
-    elif opt == '-a' or opt == '--addr':
-        OWN_ADDR = arg
+        self.init()
 
-# TODO: these were only added for convenience, should be removed in a future commit
-NET_PATH = "../network/files/"
-OWN_ADDR = "A"
-# TODO_END
+        self.initKeys()
 
-if (NET_PATH[-1] != '/') and (NET_PATH[-1] != '\\'):
-    NET_PATH += '/'
+    def init(self):
+        '''try:
+            opts, args = getopt.getopt(sys.argv[1:], shortopts='hp:a:', longopts=[
+                'help', 'path=', 'addr='])
+        except getopt.GetoptError:
+            print('Usage: python receiver.py -p <network path> -a <own addr>')
+            sys.exit(1)
+        
+        for opt, arg in opts:
+            if opt == '-h' or opt == '--help':
+                print('Usage: python receiver.py -p <network path> -a <own addr>')
+                sys.exit(0)
+            elif opt == '-p' or opt == '--path':
+                NET_PATH = arg
+            elif opt == '-a' or opt == '--addr':
+                OWN_ADDR = arg
+        '''
+        if (self.netPath[-1] != '/') and (self.netPath[-1] != '\\'):
+            self.netPath += '/'
 
-if not os.access(NET_PATH, os.F_OK):
-    print('Error: Cannot access path ' + NET_PATH)
-    sys.exit(1)
+        if not os.path.exists(self.netPath):
+            os.makedirs(self.netPath)
 
-if len(OWN_ADDR) > 1:
-    OWN_ADDR = OWN_ADDR[0]
+        if len(self.ownAddr) > 1:
+            self.ownAddr = self.ownAddr[0]
 
-if OWN_ADDR not in network_interface.addr_space:
-    print('Error: Invalid address ' + OWN_ADDR)
-    sys.exit(1)
+        if self.ownAddr not in network_interface.addr_space:
+            print('Error: Invalid address ' + self.ownAddr)
+            sys.exit(1)
 
-global privKey
-global pubKey
-if not os.path.exists("privKey.pem"):
-    privKey=RSA.generate(1024)
-    pubKey=privKey.public_key()
-    with open("privKey.pem", "wb") as handle:
-        handle.write(privKey.export_key('PEM'))
-else:
-    with open("privKey.pem", "r") as handle:
-        privKey = RSA.import_key(handle.read())
+        self.netif = network_interface(self.netPath, self.ownAddr)
 
-if not os.path.exists("../client/pubKey.pem"):
-    with open("../client/pubKey.pem", "wb") as handle:
-        handle.write(pubKey.export_key("PEM"))
-# main loop
-netif = network_interface(NET_PATH, OWN_ADDR)
-print('Main loop started...')
+    def initKeys(self):
+        if not os.path.exists("privKey.pem"):
+            self.privKey = RSA.generate(1024)
+            self.pubKey = self.privKey.public_key()
+            with open("privKey.pem", "wb") as handle:
+                handle.write(self.privKey.export_key('PEM'))
+        else:
+            with open("privKey.pem", "r") as handle:
+                self.privKey = RSA.import_key(handle.read())
 
-def handleExchange(msg):
-    if len(msg) != 32+1024:
-        return False
-    exchangeSource = msg[0:32]
-    zero = bytes.fromhex("00")
-    i = 31
-    while exchangeSource[i] == 0:
-        i -= 1
-    exchangeSource = exchangeSource[:i + 1].decode("ascii")
+        if not os.path.exists("../client/pubKey.pem"):
+            with open("../client/pubKey.pem", "wb") as handle:
+                handle.write(self.pubKey.export_key("PEM"))
 
-    exchanger = ServerKeyExchanger(netif, privKey, exchangeSource, msg[32:])
-    symKey = exchanger.serverExchangeKey()
-    print(symKey)
-    return True
+    def handleExchange(self, msg):
+        if len(msg) != 32+1024:
+            return False
+        try:
+            msg[0:32].decode("ascii")
+        except:
+            return False
+        exchangeSource = msg[0:32]
+        zero = bytes.fromhex("00")
+        i = 31
+        while exchangeSource[i] == 0:
+            i -= 1
+        exchangeSource = exchangeSource[:i + 1].decode("ascii")
 
+        exchanger = ServerKeyExchanger(self.netif, self.privKey, exchangeSource, msg[32:])
+        symKey = exchanger.serverExchangeKey()
+        print(symKey)
+        return True
 
-while True:
-    status, msg = netif.receive_msg(blocking=True)
-    if handleExchange(msg):
-        continue
+    def run(self):
+        print('Main loop started...')
+        while True:
+            status, msg = self.netif.receive_msg(blocking=True)
+            if self.handleExchange(msg):
+                continue
 
